cbde5318c8a6ee6baeb8803bfba2a942cfd9e3b5
SeoungYeop-Baek <tmdduq1999@naver.com>
2024-01-12 18:01:28 +0900

    4day

--- Problem_2751.java
+++ Problem_2751.java
@@ -6,6 +6,27 @@ import java.util.Collections;
 
 public class Problem_2751 {
     public static void main(String[] args) throws Exception {
+//        InputStream in = System.in;
+//        InputStreamReader reader = new InputStreamReader(in);
+//        BufferedReader br = new BufferedReader(reader);
+//
+//        int N = Integer.parseInt(br.readLine());
+//        int[] num = new int[N];
+//
+//        for (int i = 0; i < N; i++) {
+//            num[i] = Integer.parseInt(br.readLine());
+//        }
+//
+//        Arrays.parallelSort(num);
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (int i : num) {
+//            sb.append(i).append('\n');
+//        }
+//
+//        System.out.print(sb.toString());
+
+
         InputStream in = System.in;
         InputStreamReader reader = new InputStreamReader(in);
         BufferedReader br = new BufferedReader(reader);
